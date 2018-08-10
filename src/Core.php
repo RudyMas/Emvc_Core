ec3d4708b13c91fef701453ff7cac98aef17c888
Rudy Mas <rudy.mas@rudymas.be>
2018-08-10 12:13:47 +0200

    Removed unused packages from composer.json file

--- src/Core.php
+++ src/Core.php
@@ -15,7 +15,7 @@ use RudyMas\PDOExt\DBconnect;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.2.1.9
+ * @version     0.3.0.0
  * @package     EasyMVC\Core
  */
 class Core
