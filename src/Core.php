f7c89a2562eed5535f350a90f71a928e3504aeac
Rudy Mas <rudy.mas@rudymas.be>
2018-10-06 18:18:53 +0200

    Updated package to use EasyMVC Menu version 2.*

--- src/Core.php
+++ src/Core.php
@@ -15,7 +15,7 @@ use RudyMas\PDOExt\DBconnect;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.4.0.12
+ * @version     0.4.0.13
  * @package     EasyMVC\Core
  */
 class Core
