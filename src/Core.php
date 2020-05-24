c281ae1127a7944a929b631a7ae50fc18910385b
Rudy Mas <rudy.mas@rudymas.be>
2020-05-24 18:16:26 +0200

    Update use DBconnect

--- src/Core.php
+++ src/Core.php
@@ -4,7 +4,7 @@ namespace EasyMVC;
 
 use Exception;
 use RudyMas\Manipulator\Text;
-use RudyMas\PDOExt\DBconnect;
+use RudyMas\DBconnect;
 
 /**
  * Class Core (PHP version 7.2)
@@ -12,7 +12,7 @@ use RudyMas\PDOExt\DBconnect;
  * @author Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright 2018-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 0.5.2.0
+ * @version 0.5.2.1
  * @package EasyMVC
  */
 class Core
